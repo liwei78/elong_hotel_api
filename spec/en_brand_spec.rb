c7cb448747ecddd9f406162fd1b2ccf974be197b
刘明 <charles.liu@corp.elong.com>
2014-05-30 14:07:16 +0800

    shrink the test file sizes

--- spec/en_brand_spec.rb
+++ spec/en_brand_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ElongHotelApi do
   subject{ElongHotelApi.new(cn_url_brand: $en_sample_brand)}
   describe '#brands' do
-    specify{subject.brands.size.should == 8564}
+    specify{subject.brands.size.should == 11}
     specify{subject.brands.first.should == [id:"32", name:"", group_id:"0", short_name:"Home Inn", letters:"Home Inn"] }
   end
 end
--- spec/en_geo_spec.rb
+++ spec/en_geo_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 describe ElongHotelApi do
   subject{ElongHotelApi.new(en_url_geo: $en_sample_geo,lang: 'en')}
   describe '#cities' do
-    specify{subject.cities.size.should == 1321}
+    specify{subject.cities.size.should == 3}
     specify{subject.cities.first.should == [name:"Beijing", id:'0101']}
   end
 
   describe '#districts' do
-    specify{subject.districts.size.should == 1321}
+    specify{subject.districts.size.should == 3}
     specify{subject.districts['0101'].first.should == [name:'Changping',id:'0028']}
   end
 
   describe '#centers' do
-    specify{subject.centers.size.should == 1321}
+    specify{subject.centers.size.should == 3}
     specify{subject.centers['0101'].first.should == [name:'Anzhen',id:'010186']}
   end
 
   describe '#locations' do
-    specify{subject.locations.size.should == 1321}
+    specify{subject.locations.size.should == 3}
     specify{subject.locations['0101'].first.should == [id:'0005',name:'Oriental Plaza']}
   end
 end
